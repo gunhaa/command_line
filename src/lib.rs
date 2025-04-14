0399abde1dadb92a4a1b742989eb2fbf27202b65
gunha <wh8299@gmail.com>
2025-04-14 21:43:25 +0900

    refactoring : functional programming 방향으로 리팩토링

--- src/lib.rs
+++ src/lib.rs
@@ -26,15 +26,27 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>, ) -> Result<Config, &'static str> {
+        // if args.len() < 3 {
+        //     return Err("not enough arguments");
+        // }
 
-        // 비효율적이지만 가장 직관적
-        let query = args[1].clone();
-        let file_path= args[2].clone();
+        // // 비효율적이지만 가장 직관적
+        // let query = args[1].clone();
+        // let file_path= args[2].clone();
         
+        println!("args[0] : {:?}", args.next());
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next(){
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {query, file_path, ignore_case})
@@ -74,15 +86,20 @@ Trust me.";
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
-    let mut results = Vec::new();
+    // let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
 
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+
+    // results
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
